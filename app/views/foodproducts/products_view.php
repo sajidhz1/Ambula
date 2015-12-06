f3f729d057ab0eb0d00c8a26db52b888c0261916
sajidhz1 <sajidhzazahir@gmail.com>
2015-12-06 16:33:17 +0530

    updated product view

--- app/views/foodproducts/products_view.php
+++ app/views/foodproducts/products_view.php
@@ -1,9 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sajidhz
- * Date: 12/2/2015
- * Time: 3:42 PM
- */
+<!DOCTYPE html>
+<html>
 
-echo "lalala";
+<head>
+    <meta charset="utf-8">
+    <title>The Ambula</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="http://localhost/Ambula/public/css/bootstrap.css" rel="stylesheet" media="screen"/>
+    <link href="http://localhost/Ambula/public/css/custom.css" rel="stylesheet" media="screen"/>
+    <link href="http://localhost/Ambula/public/css/color1.css" rel="stylesheet" media="screen"/>
+
+    <!-- fav icon -->
+    <link rel="icon" href="http://localhost/Ambula/public/img/fav_ico.png" type="image/gif" sizes="16x16">
+
+    <script type="text/javascript" src="http://localhost/Ambula/public/js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="http://localhost/Ambula/public/js/typeahead.js"></script>
+
+    <script src="http://localhost/Ambula/public/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="css/font-awesome-ie7.min.css"></script>
+    <![endif]-->
+
+    <!-- Google Font Link: Choose font you want on google font(http://www.google.com/webfonts) and make sure your replace those fonts in name in custom.css -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Sanchez:400,400italic' rel='stylesheet'
+          type='text/css'>
+
+    <!-- ##### Le HTML5 shim, for IE6-8 support of HTML5 elements ##### -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+</head>
+
+<body>
+
+<!--Header START -->
+<?php $this->view('_template/navigation_menu', "newRecipe") ?>
+<div class="container-fluid" style="background: #666">
+
+    <div class="row" style="height: 250px;background: #bbb;margin-top: 50px; ">ndj hehf </div>
+    <div  class="row" style="height: 300px;background: #eee;">ndj hehf </div>
+</div>
+
+</body>
+</html>
