5339dae7f3ac0e4eb13b3d8c0670b0afe3d2d4b3
sajidhz1 <sajidhzazahir@gmail.com>
2015-12-06 21:16:31 +0530

    updated product view

--- app/views/foodproducts/products_view.php
+++ app/views/foodproducts/products_view.php
@@ -38,20 +38,81 @@
         .box .inner {
             background-color: #eee;
         }
+
+        header.carousel {
+            height: 50%;
+        }
+
+        header.carousel .item,
+        header.carousel .item.active,
+        header.carousel .carousel-inner {
+            height: 100%;
+        }
+
+        header.carousel .fill {
+            width: 100%;
+            height: 100%;
+            background-position: center;
+            background-size: cover;
+        }
+
     </style>
+
 </head>
 
 <body>
 
 <!--Header START -->
 <?php $this->view('_template/navigation_menu', "newRecipe") ?>
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide" style="margin-top:50px;height: 250px;">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 1</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 2</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 3</h2>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+</header>
+
 <div class="container-fluid">
 
-    <div class="row" style="height: 250px;background: #bbb;margin-top: 50px; ">ndj hehf </div>
+
+
+
     <div  class="row" style=" margin-top: 15px;">
         <ul class="nav nav-tabs">
             <li class="active"><a data-toggle="tab" href="#home">Latest</a></li>
-            <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+            <li><a data-toggle="tab" href="#menu1">Brands</a></li>
             <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
             <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
         </ul>
@@ -155,7 +216,7 @@
                 </div>
             </div>
 
- 
+
         </div>
         <div id="menu1" class="tab-pane fade">
             <h3>Menu 1</h3>
@@ -174,6 +235,17 @@
 
     <div  class="row" style="height: 300px;background: #eee;margin-top: 50px;">ndj hehf </div>
 </div>
+
+<script type="text/javascript" src="http://localhost/Ambula/public/js/jquery-1.11.0.min.js"></script>
+
+<script src="http://localhost/Ambula/public/js/bootstrap.min.js"></script>
+
+<!-- Script to Activate the Carousel -->
+<script>
+    $('.carousel').carousel({
+        interval: 4000 //changes the speed
+    })
+</script>
 
 </body>
 </html>
